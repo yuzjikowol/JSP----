6132243578998909016c893cbf762a98d795eb0b
Hayato <yutokashinat743@gmail.com>
2016-07-28 21:54:01 +0900

    STS練習

---
+++ DemoApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+import org.springframework.boot.SpringApplication;
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+       SpringApplication.run(DemoApplication.class,args);
+	}
+
+}
